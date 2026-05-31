1cb390c1f5c4f7ac37c081094faa1b34b1cf37ae
ndjp78m8tm-netizen <ndjp78m8tm@privaterelay.appleid.com>
2026-05-31 19:54:06 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,46 @@
+# File: initials.py
+# Description: Print out my initials in stylized large letters.
+# Assignment Number: 1
+#
+# Name: Christian Nii Anertey Abbey
+# STUDENT ID: 2425404999
+# Email: abbeychristian2@gmail.com
+# Grader: Augustus
+#
+# On my honor, Christian Nii Anertey Abbey, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    """Prints small initials and stylized large block letters for CNA."""
+    # Print a blank line followed by small initials and another blank line
+    print()
+    print("...CNA")
+    print()
 
+    # Line 1 of the large letters
+    print("...CCCCCCCCC........NNNN....NNNN........AAAAAAAA........")
+    # Line 2 of the large letters
+    print("...CCCCCCCCCC.......NNNNN...NNNN.......AAAAAAAAAA.......")
+    # Line 3 of the large letters
+    print("...CCCC...CCCC......NNNNN...NNNN......AAAA....AAAA......")
+    # Line 4 of the large letters
+    print("...CCCC.............NNNNNN..NNNN.....AAAA......AAAA.....")
+    # Line 5 of the large letters
+    print("...CCCC.............NNNN.NN.NNNN.....AAAAAAAAAAAAAA.....")
+    # Line 6 of the large letters
+    print("...CCCC.............NNNN..NNNNNN.....AAAAAAAAAAAAAA.....")
+    # Line 7 of the large letters
+    print("...CCCC.............NNNN...NNNNN.....AAAA......AAAA.....")
+    # Line 8 of the large letters
+    print("...CCCC...CCCC......NNNN...NNNNN.....AAAA......AAAA.....")
+    # Line 9 of the large letters
+    print("...CCCCCCCCCC...**..NNNN....NNNN...**AAAA......AAAA...**")
+    # Line 10 of the large letters
+    print("...CCCCCCCCC....**..NNNN....NNNN...**AAAA......AAAA...**")
+
+    # Print final blank line
+    print()
 
 
 main()
